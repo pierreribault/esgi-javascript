d6d4e767d6f72a579b498962bb7e98d50ad81e35
Pierre Ribault <contact@pierreribault.fr>
2020-10-21 18:59:13 +0200

    [DONE] exercice 2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,9 +1,17 @@
 function type_check_v1(arg1, arg2) {
+    if(arg2 === "null") {
+        return arg1 === null
+    }
+
+    if(arg2 === "array") {
+        return Array.isArray(arg1)
+    }
+
     return typeof(arg1) === arg2
 }
 
 function type_check_v2(arg, object) {
-    if(object.hasOwnProperty('type') && (typeof(arg) !== object.type)) {
+    if(object.hasOwnProperty('type') && !type_check_v1(arg, object.type)) {
         return false
     }
 
@@ -18,14 +26,32 @@ function type_check_v2(arg, object) {
     return true
 }
 
-function type_check() {
+function type_check(object, conf) {
+    if(!conf.hasOwnProperty('type')) {
+        return false
+    }
+    
+    if(typeof(object) !== conf.type) {
+        return false
+    }
 
+    if(conf.hasOwnProperty('properties')) {
+        for(let propertyName in conf) {
+            console.log(propertyName)
+        }
+    }
+
+    return true
 }
 
 console.log(type_check_v1(1, "number"))
+console.log(type_check_v1(null, "null"))
+console.log(type_check_v1([2,3], "array"))
 console.log(type_check_v2(1, {type: "string"}))
 console.log(type_check_v2(1, {type: "number", value: 1}))
 console.log(type_check_v2(1, {type: "string", value: 1}))
 console.log(type_check_v2(1, {type: "number", value: 1, enum: 4}))
 console.log(type_check_v2(1, {type: "number", value: 1, enum: [1,2]}))
 console.log(type_check_v2(1, {type: "number", value: 1, enum: [3,4]}))
+
+console.log(type_check({props1: 3}, {type: "object", properties: {props1: 3}}))
