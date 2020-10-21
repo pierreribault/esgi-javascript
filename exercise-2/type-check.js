dc3a68fefaf3c181833f31460ac3dbc24d63737a
Pierre Ribault <contact@pierreribault.fr>
2020-10-21 19:15:43 +0200

    [DONE] exercice 2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -11,6 +11,10 @@ function type_check_v1(arg1, arg2) {
 }
 
 function type_check_v2(arg, object) {
+    if(!type_check_v1(object, "object")) {
+        return false
+    }
+
     if(object.hasOwnProperty('type') && !type_check_v1(arg, object.type)) {
         return false
     }
@@ -27,6 +31,10 @@ function type_check_v2(arg, object) {
 }
 
 function type_check(object, conf) {
+    if(!type_check_v1(object, "object") || !type_check_v1(conf, "object")) {
+        return false
+    }
+
     if(!conf.hasOwnProperty('type')) {
         return false
     }
@@ -36,8 +44,10 @@ function type_check(object, conf) {
     }
 
     if(conf.hasOwnProperty('properties')) {
-        for(let propertyName in conf) {
-            console.log(propertyName)
+        for(let propertyName in conf.properties) {
+            if(object.hasOwnProperty(propertyName) && !type_check_v2(object[propertyName], conf.properties[propertyName])) {
+                return false
+            }
         }
     }
 
@@ -54,4 +64,4 @@ console.log(type_check_v2(1, {type: "number", value: 1, enum: 4}))
 console.log(type_check_v2(1, {type: "number", value: 1, enum: [1,2]}))
 console.log(type_check_v2(1, {type: "number", value: 1, enum: [3,4]}))
 
-console.log(type_check({props1: 3}, {type: "object", properties: {props1: 3}}))
+console.log(type_check({props1: 3}, {type: "object", properties: {props1: {type: "number"}}}))
