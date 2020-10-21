be0893f80cf7c9d3de001c769994ba92d857108d
Pierre Ribault <contact@pierreribault.fr>
2020-10-21 09:00:56 +0200

    [DONE] exercice 1

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -2,21 +2,24 @@ function camelCase(string) {
     if(string == null || string === 'object') {
         return string
     }
-    
+
     return string.replace('_', ' ').split(' ').map(word => toUpperCase(word)).join('')
 }
 
 function prop_access(object, path) {
     let result = object
 
-    if(path == null || path == "" || object == null) {
+    if(path == null || path == "") {
         return object
     }
 
+    if(object == null) {
+        return "test not exist"
+    }
+
     path.split('.').forEach(element => {
-        if(result[element] == undefined) {
-            result = path + " not exist"
-            return
+        if(result[element] === undefined) {
+            return result = path + " not exist"
         }
 
         result = result[element]
@@ -28,3 +31,18 @@ function prop_access(object, path) {
 function toUpperCase(string) {
     return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase()
 }
+
+const prairie = {
+    'animal' : {
+        'region': "france",
+        'couleur': ["bleu", "blanc", "rouge"],
+        'type' : {
+            'name': 'chien'
+        }
+    }
+}
+
+
+console.log(toUpperCase('un test'))
+console.log(camelCase('un test'))
+console.log(prop_access(prairie, "animal.test"))
